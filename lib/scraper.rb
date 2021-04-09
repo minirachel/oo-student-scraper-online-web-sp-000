32d1b4c7ed59e719044c093ece4b769a77c0ed3e
minirachel <rachel.a.levinson@gmail.com>
2021-04-09 12:16:14 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,24 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+
+    site = Nokogiri::HTML(open(index_url))
+
+    students = {}
+
+      site.css("div.roster-cards-container").each do |student|
+
+          students << {
+            :name => student.css("h4.student-name").text,
+            :location => student.css("p.student-location").text,
+            :profile_url => student.css(site.css("div.student-card a").attribute("href").value
+            } 
+      end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +26,3 @@ class Scraper
   end
 
 end
-
